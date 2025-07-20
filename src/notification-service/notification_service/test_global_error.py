ac2a7e3e71bf47ad608bedcbf19a60c7d81daf74
Jojees <joji@jojees.net>
2025-07-20 16:53:53 +0530

    feat(CI/Testing): Added Testing Strategy and Unit test step into Github Actions pipeline. * Added documentation on the testing strategy. * Updated all applications with pytest & pytest-cov as a dev dependency. * Removed arbitary test files from notification service. * Added sample unit test for all applications.

--- src/notification-service/notification_service/test_global_error.py
+++
@@ -1,9 +0,0 @@
-my_global_var = None
-
-def my_func():
-    # This setup *should* cause a SyntaxError if Python behaves as expected
-    print(my_global_var) # First use
-    global my_global_var # Global declaration after use
-    my_global_var = 1
-
-my_func()
---
+++ src/notification-service/tests/test_basic.py
@@ -0,0 +1,2 @@
+def test_example_assertion():
+    assert 1 + 1 == 2
